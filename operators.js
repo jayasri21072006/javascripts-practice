370746d0c313401477c50d4daa3192360dd90af1
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-03-14 18:46:13 +0530

    Create operators.js

---
+++ operators.js
@@ -0,0 +1,57 @@
+comment  
+
+
+single line  // comment basics
+
+
+double line  /* ...... etc   */
+
+eg:
+var fruitname="banana"
+var count=12
+var price=100
+var total=count*price
+console.log(fruitname)
+/*ahgwydgdf
+*/
+console.log(total)
+
+
+
+operator:
+post icremenrt a++
+post decrementg a--
+
+
+var a=10
+var b=a++
+console.log(b)
+console.log(a)
+
+10
+11 
+val of a  remanis same no increment untill it seees next a   for posdt increment and post decrement
+
+var a=10
+var b=a--
+console.log(b)
+console.log(a)
+
+10
+9
+
+val of a  changes  increment and not wait untill  it seees next a   for pre increment and pre decrement
+
+
+
+var a=10
+var b=++a
+console.log(b)
+console.log(a)
+
+
+11
+11
+
+
+
