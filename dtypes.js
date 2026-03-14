15bba3c2ae7eda00a938e4bd55d98331117381fe
Jayasri T <143895835+jayasri21072006@users.noreply.github.com>
2026-03-14 18:51:01 +0530

    Create dtypes.js

---
+++ dtypes.js
@@ -0,0 +1,35 @@
+dats types 
+
+1.primitive datastypes
+
+console.log(typeof(10))
+
+number
+
+
+console.log(typeof("jayasri"))
+
+string
+
+
+var a=null
+console.log(typeof(a))
+
+
+object
+
+
+var a
+console.log(typeof(a))
+
+undefined
+
+var a=true
+console.log(typeof(a)
+
+boolean
+
+
+
+
+2.non primitive
